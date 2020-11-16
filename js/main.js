40a5561e765332b7a34ebefb6859337d51f6c3b8
pf4pad <65763670+pf4pad@users.noreply.github.com>
2020-11-16 22:15:46 +0200

    main.js

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+    $(window).load(function () {
+      $(".before-after").twentytwenty({
+        before_label: 'Без скинали', // Set a custom before label
+        after_label: 'Со скинали', // Set a custom after label
+      });
+      $(".before-slider").slick({
+        draggable: false,
+        swipeToSlide: false,
+        dots: true,
+        dotsClass: 'before-slider__dots',
+        prevArrow: $('.arrow-left'),
+        nextArrow: $('.arrow-right')
+      });
+      $('.menu-button').on('click', function () {
+        $('.menu').toggleClass('menu_active');
+      });
+      $('.select').on('click', function () {
+        $('.select__dropdown').toggleClass('.select__dropdown_open');
+      });
+    });
