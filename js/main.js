e093a5a9190f1d87a480bb22be7386daec0ed46c
pf4pad <65763670+pf4pad@users.noreply.github.com>
2020-11-21 18:03:35 +0200

    reviews

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 
-    $(window).load(function () {
-      $(".before-after").twentytwenty({
+    $(window).load(function (){ 
+       $(".before-after").twentytwenty({
         before_label: 'Без скинали', // Set a custom before label
-        after_label: 'Со скинали', // Set a custom after label
+        after_label: 'Со скинали' // Set a custom after label
         
       });
+    });
       $(".before-slider").slick({
         draggable: false,
         
@@ -26,4 +27,38 @@
        var value = $(this).attr('data-value');
        console.log(value);
         });
+
+
+  $(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+  });
+  
+$(".reviews-slider").slick({
+        draggable: false,
+        
+        swipe:false,
+        dotsClass: 'before-slider__dots',
+        dots: true
+        
+        
+        //prevArrow: $('.arrow-left'),
+        //nextArrow: $('.arrow-right')
+      });
+      
+
     });
